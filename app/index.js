4179449d0882835baa13c7deb9d64a7e60f41c04
Tyler Small <email": null>
2017-01-12 16:28:18 -0600

    Change package requirements to debug dependencies.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,53 @@
-var noble = require('noble');
-console.log(noble);
-noble.startScanning();
+// var noble = require('noble');
+// console.log(noble);
+// noble.startScanning();
 
-noble.on('scanStart', function(){
-  console.log("Scanning...");
+// noble.on('stateChange', function(){
+//   console.log("Scanning...");
+// });
+// console.log("Hello World!");
+'use strict';
+
+var RollingSpider = require('rolling-spider');
+var temporal = require('temporal');
+var rollingSpider = new RollingSpider({uuid: 'RS_B138965'});
+
+rollingSpider.connect(function () {
+  rollingSpider.setup(function () {
+    rollingSpider.flatTrim();
+    rollingSpider.startPing();
+    rollingSpider.flatTrim();
+
+    temporal.queue([
+      {
+        delay: 3000,
+        task: function () {
+          console.log('Takeoff');
+          rollingSpider.takeOff();
+          rollingSpider.flatTrim();
+        }
+      },
+      {
+        delay: 2000,
+        task: function () {
+          console.log('Forward');
+          rollingSpider.forward();
+        }
+      },
+      {
+        delay: 3000,
+        task: function () {
+          rollingSpider.land();
+          console.log('Land');
+        }
+      },
+      {
+        delay: 3000,
+        task: function () {
+          temporal.clear();
+          process.exit(0);
+        }
+      }
+    ]);
+  });
 });
-console.log("Hello World!");
