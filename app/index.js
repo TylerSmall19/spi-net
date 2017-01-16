c0232c5e53f8760dbcde0599d384ba55de27256b
Tyler Small <email": null>
2017-01-16 12:32:12 -0600

    Enable support for accepting channel messages from PubNub.

--- app/index.js
+++ app/index.js
@@ -8,50 +8,32 @@
 // console.log("Hello World!");
 'use strict';
 
-var RollingSpider = require('rolling-spider');
-var temporal = require('temporal');
-var rollingSpider = new RollingSpider();
+// var RollingSpider = require('rolling-spider');
+// var temporal = require('temporal');
+// var rollingSpider = new RollingSpider();
 // Kevin
 // {uuid: 'RS_B138965'}
 // Jon
 // {uuid: 'RS_R042799'}
 
-rollingSpider.connect(function () {
-  console.log('Paired');
-  rollingSpider.setup(function () {
-    rollingSpider.flatTrim();
-    rollingSpider.startPing();
-    rollingSpider.flatTrim();
-    temporal.queue([
-      {
-        delay: 5000,
-        task: function () {
-          console.log('Takeoff');
-          rollingSpider.takeOff();
-          rollingSpider.flatTrim();
-        }
-      },
-      {
-        delay: 5000,
-        task: function () {
-          console.log('Forward');
-          rollingSpider.forward();
-        }
-      },
-      {
-        delay: 3000,
-        task: function () {
-          rollingSpider.land();
-          console.log('Land');
-        }
-      },
-      {
-        delay: 3000,
-        task: function () {
-          temporal.clear();
-          process.exit(0);
-        }
-      }
-    ]);
-  });
+var pubnub = require("pubnub")({
+    ssl           : true,  // <- enable TLS Tunneling over TCP
+    publish_key   : "pub-c-5c099dc9-4fb8-41d4-a462-54224a4f47d7",
+    // publish_key   : [YOUR_PUBNUB_PUB_KEY_HERE],
+    subscribe_key : "sub-c-78d941e8-d9b7-11e6-b9cf-02ee2ddab7fe"
+    // subscribe_key : [YOUR_PUBNUB_SUB_KEY_HERE]
 });
+
+pubnub.subscribe({
+  channel  : "sky_net",
+  callback : function(message) {
+    console.log('Received: ', message);
+  }
+});
+
+// rollingSpider.connect(function () {
+//   console.log('Paired');
+//   rollingSpider.setup(function () {
+//     // Logic to run commands here
+//   });
+// });
